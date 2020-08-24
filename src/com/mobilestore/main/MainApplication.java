df83c22e3ad46648794445118c85c4d214295fab
nikesh <nikeshprasad9@gmail.com>
2020-08-24 17:05:29 +0530

    Main class for user interaction

--- src/com/mobilestore/main/MainApplication.java
+++ src/com/mobilestore/main/MainApplication.java
@@ -25,13 +25,16 @@ public class MainApplication {
 			System.out.println("3. Insert Sales");
 			System.out.println("4. View Sales Report");
 			System.out.println("0. Exit");
+			/*
+			More operations to be added later
+			*/
 			
 			System.out.print("Enter your Choice: ");
 			choice = Integer.parseInt(br.readLine());
 			
 			switch (choice) {
 			case 1:	Mobile mobile = new Mobile();
-					System.out.print("Enter the brand name: ");
+					System.out.print("Enter brand name: ");
 					mobile.setBrand(br.readLine());
 					System.out.print("Enter mobile name: ");
 					mobile.setMobileName(br.readLine());
@@ -41,7 +44,7 @@ public class MainApplication {
 					mobile.setUnitPrice(Double.parseDouble(br.readLine()));
 					
 					String result = admin.insertStock(mobile);
-					if(result.length() == 6) System.out.println("Mobile inserted successfully in the stock with id: " + result);
+					if(result.length() == 6) System.out.println(mobile.getMobileName() + " added to stock with id: " + result);
 					else System.out.println(result);
 					break;
 					
@@ -58,7 +61,7 @@ public class MainApplication {
 					System.out.print("Enter date (dd-mm-yyyy): ");
 				    Date date = dateFormat.parse(br.readLine()); 
 					salesObject.setSalesDate(date);
-					System.out.print("Enter product id: ");
+					System.out.print("Enter mobile id: ");
 					salesObject.setMobileId(br.readLine());
 					System.out.print("Enter quantity sold: ");
 					salesObject.setQuantitySold(Integer.parseInt(br.readLine()));
@@ -70,14 +73,19 @@ public class MainApplication {
 					
 			case 4:	salesReport = admin.getSalesReport();
 					Iterator<SalesReport> iter = salesReport.listIterator();
-					System.out.printf("| %-12s | %-12s | %-12s | %-12s | %-22s | %-14s | %-16s | %-18s | %-16s |%n", "Sales ID", "Sales Date", "Brand", "Mobile Id", "Mobile Name", "Quantity sold", "Price per unit", "Sales price/unit", "Profit amount");
+					System.out.printf("| %-12s | %-12s | %-12s | %-12s | %-22s | %-14s | %-16s | %-18s | %-16s | %-16s |%n", 
+							"Sales ID", "Sales Date", "Brand", "Mobile Id", "Mobile Name", "Quantity sold", "Price/unit", "Sales price/unit", "Profit/unit", "Total profit");
+					
 					while(iter.hasNext()) {
 						SalesReport sr = iter.next();
-						System.out.printf("| %-12s | %-12s | %-12s | %-12s | %-22s | %-14d | %-16.2f | %-18.2f | %-16.2f |%n", sr.getSalesID(), dateFormat.format(sr.getSalesDate()), sr.getBrand(), sr.getMobileId(), sr.getMobileName(), sr.getQuantitySold(), sr.getUnitPrice(), sr.getSalesPricePerUnit(), sr.getProfitAmount());
+						System.out.printf("| %-12s | %-12s | %-12s | %-12s | %-22s | %14d | %16.2f | %18.2f | %16.2f | %16.2f |%n",
+								sr.getSalesID(), dateFormat.format(sr.getSalesDate()), sr.getBrand(), sr.getMobileId(), sr.getMobileName(),
+								sr.getQuantitySold(), sr.getUnitPrice(), sr.getSalesPricePerUnit(), sr.getProfitPerUnit(), sr.getTotalProfit());
 					}
 					break;
 					
-			case 0: System.out.println("Exiting...");
+					
+			case 0: System.out.println("Exiting");
 					break;
 					
 					
