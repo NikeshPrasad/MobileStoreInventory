0d03af3f5bd5fd9a09e18dfa0b5aee08a9cb2430
nikesh <nikeshprasad9@gmail.com>
2020-08-24 17:58:58 +0530

    Execute last. Populate Stock table with few mobiles

---
+++ sql-src/insert_values.sql
@@ -0,0 +1,4 @@
+INSERT INTO Stock(  MobileId, Brand, MobileName, QuantityOnHand, UnitPrice)
+    VALUES  ('RE1001', 'Redmi', 'REDMI Note 3', 20, 12000),
+            ('IP1002', 'Apple', 'Iphone 5S', 10, 21000),
+            ('PA1003', 'Panasonic', 'Panasonic P55', 50, 5500);
